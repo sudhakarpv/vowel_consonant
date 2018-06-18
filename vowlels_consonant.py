5487a594ed8db196a93ae09570d64847f175bad0
sudhakarpv <40256682+sudhakarpv@users.noreply.github.com>
2018-06-18 11:56:14 +0530

    Update and rename README.md to vowlels_consonant.py

---
+++ vowlels_consonant.py
@@ -0,0 +1,10 @@
+# vowel_consonant
+user=(input("Enter Any Alphabet= "))
+if(user=='A'or user=='a'or user=='E'or user=='e'or user=='I'or user=='i'or user=='O'or user=='o'
+   or user=='U'or user=='u'):
+        print(user,"is a Vowel")
+elif (user==int):
+        print("Invalid")
+else:
+        print (user,"is a Consonant")
+        
